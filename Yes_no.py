9fc3c5c80a85573fdf85ce00a98c512495249152
mcleod.mya <mcleod.mya@awatapu.school.nz>
2021-09-15 14:19:21 +1200

    START

--- Yes_no.py
+++ Yes_no.py
@@ -2,12 +2,12 @@
 show_instructions = input("Have you played before?").lower()
 
 #if they say yes, output 'program  contiune'
-if show_instructions == "yes" or show_instructions == "y", show_instructions == "Yes":
+if show_instructions == "yes" or show_instructions == "y":
     print("Program continues!")
 
 #if they say No, output 'program  contiune'
-if show_instructions == "no" or show_instructions == "n", show_instructions == "No":
-    print("Display the instructions!")
+if show_instructions == "no" or show_instructions == "n":
+    print("Welcome to Zoot's basic maths! You will be learning math in 4 sections, each section has 5 questions. Remeber to have fun! ZOOT ZOOT!")
 
 #if they say other, output 'Please answer the question'
 elif show_instructions == "maybe":
