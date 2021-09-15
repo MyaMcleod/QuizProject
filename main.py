e3977edef3f518644d7ebf6732041f854081adbe
mcleod.mya <mcleod.mya@awatapu.school.nz>
2021-09-15 15:05:34 +1200

    START

---
+++ main.py
@@ -0,0 +1,34 @@
+
+def name():
+ print(name)
+
+
+
+
+
+
+
+
+
+
+
+def instructions():
+    #Ask user if they have played before
+    show_instructions = input("Have you played before?").lower()
+
+    #if they say yes, output 'program  contiune'
+    if show_instructions == "yes" or show_instructions == "y":
+        print("Program continues!")
+
+    #if they say No, output 'program  contiune'
+    if show_instructions == "no" or show_instructions == "n":
+        print("Welcome to Zoot's basic maths! You will be learning math in 4 sections, each section has 5 questions. Remeber to have fun! ZOOT ZOOT!")
+
+    #if they say other, output 'Please answer the question'
+    elif show_instructions == "maybe":
+        print("Please answer the question with yes or no!")
+
+    #if they say no, output 'display instructions'
+    else:
+        print()
+print(instructions())
